60db82b1cdb8411cd65b2a290f136eb611fbd329
brunomffl <brunocardoso07@Outlook.com>
2026-05-21 19:36:43 -0300

    bff/create-server

---
+++ src/middlewares/error-handling.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "@/utils/AppError";
+import { ZodError } from "zod";
+
+export function errorHandling(error: any, req: Request, res: Response, next: NextFunction){
+    
+    //verifica se o erro é uma instância da nossa classe, aí vai ser um erro interno
+    if(error instanceof AppError){
+        return res.status(error.statusCode).json({ message: error.message });
+    };
+
+    if(error instanceof ZodError){
+        return res.status(400).json({
+            message: "Validation error",
+            issues: error.format()
+        });
+    };
+    
+
+    //retorna um erro genérico se nao cair em nenhuma das cond
+    return res.status(500).json({ message: error.message });
+}
